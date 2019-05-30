72f2c6138ace0349f8717d3084cda119697239e3
Daniel Inge <daninge98@gmail.com>
2019-05-29 20:45:41 -0700

    Fixed setting to prevent crashing

--- advo/settings.py
+++ advo/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'haystack',
     'tinymce',
     'ajax_select',
     'magazine',
@@ -277,3 +276,19 @@ SECURE_SSL_REDIRECT = True
 import elasticsearch
 from requests_aws4auth import AWS4Auth
 awsauth = AWS4Auth(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,'us-east-1','es')
+
+HAYSTACK_CONNECTIONS = {	
+    'default': {	
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',	
+        'URL': 'https://search-advo-search-tdbbozoee2qmgiowsicqdjyfze.us-east-1.es.amazonaws.com',	
+        'INDEX_NAME': 'haystack',	
+        'KWARGS': {	
+            'port': 443,	
+            'http_auth': awsauth,	
+            'use_ssl': True,	
+            'verify_certs': True,	
+            'connection_class': elasticsearch.RequestsHttpConnection	
+        },	
+
+     },	
+}
